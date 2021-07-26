fbf5bfd1dd5130e03e556e8a0fb58d54492340a1
Link <66062268+Link0v0@users.noreply.github.com>
2021-07-26 10:08:39 +0200

    Add files via upload

---
+++ dist/precache-manifest.7d21a16cda853ec5ceccd2b2a026ff15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499c7a549d7ba14977d4",
+    "url": "/css/app.4bbad968.css"
+  },
+  {
+    "revision": "483b1964562fbfba02d845a24c102c74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "499c7a549d7ba14977d4",
+    "url": "/js/app.302f04b5.js"
+  },
+  {
+    "revision": "1ff0b15859d70f7dec48",
+    "url": "/js/chunk-vendors.f76a461a.js"
+  },
+  {
+    "revision": "a4dcc274d6ceb0f6e74bb64d376ae285",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ src/components/Card.vue
@@ -0,0 +1,73 @@
+<template>
+	<div class="card">
+		<a :href="anime.url" target="_blank">
+			<img 
+				:src="anime.image_url" 
+				:alt="anime.title + ' Poster'" 
+			/>
+			<h3>{{ anime.title }}</h3>
+		</a>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['anime']
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+
+	width: 20%;
+	padding: 0px 8px;
+	margin-bottom: 16px;
+
+	img {
+		width: 70%;
+		height: 70%;
+		object-fit: cover;
+		border-radius: 16px;
+		box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+		transition: 0.4s;
+	}
+
+	h3 {
+		padding: auto;
+		color: #dfdfdf;
+		transition: .4s;
+	}
+
+	&:hover {
+		img {
+			transform: scale(1.05);
+		}
+
+		h3 {
+			color: #4c8fe6;
+		}
+	}
+}
+
+@media (max-width: 768px) {
+	.card {
+			width: 25%;
+}
+  .card h3 {
+			font-size: 90%;
+}
+}
+@media (max-width: 425px) {
+   .card {
+			width: 50%;
+}
+  .card h3 {
+			font-size: 80%;
+}
+}
+@media (max-width: 320px) {
+  .card h3 {
+			font-size: 60%;
+}
+}
+</style>
